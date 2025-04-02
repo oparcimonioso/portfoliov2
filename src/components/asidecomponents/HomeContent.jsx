05527391493a318975d138bc89a1884042b21d89
Guilherme Nascimento <guilherme.richardy439@gmail.com>
2025-04-02 19:32:48 -0300

    tentativa de inserção de imagem

--- src/components/asidecomponents/HomeContent.jsx
+++ src/components/asidecomponents/HomeContent.jsx
@@ -9,7 +9,7 @@ const HomeContent = () => {
       {/* Imagem */}
       <div className="relative w-48 h-48 md:w-64 md:h-64">
         <img
-          src="/public/guilherme.png"
+          src="/public/imagens/Guilherme.png"
           alt="Guilherme Nascimento"
           className={`w-full h-full object-cover rounded-full border-4 shadow-xl hover:scale-105 transition-all duration-300 ${
             isDark 
